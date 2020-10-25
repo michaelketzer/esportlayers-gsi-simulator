852312cb3a7cd89e49d5abb8062200a42b255c47
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-10-25 12:25:16 +0100

    Started implementation; Added dotagsi events db;

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
 import * as React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import FooView from "./views/FooView";
-import BarView from "./views/BarView";
-import { Layout } from "./views/Layout";
-import "./styles/main.scss";
+import fs from 'fs';
+import './App.scss';
+import { timeStamp } from "console";
+const {dialog} = require('electron').remote;
+
+interface GsiEvent {
+    fullPath: string;
+    fileName: string;
+    ts: number;
+}
+
+function useDotaEvents(): GsiEvent[] {
+    const [files, setFiles] = React.useState<GsiEvent[]>([]);
+    if(files.length === 0) {
+        dialog.showOpenDialog({
+            properties: ['openDirectory']
+            //@ts-ignore
+        }).then(({filePaths}) => {
+            const path = filePaths[0];
+            path && fs.readdir(path, (err, fileNames) => setFiles(fileNames.map((name) => ({
+                fullPath: path + '/' + name,
+                fileName: name,
+                ts: +name.substring(0, name.indexOf('.')),
+            }))));
+        });
+    }
+
+    return files;
+}
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <Switch>
-                    <Route path="/bar" component={BarView} />
-                    <Route exact path="/" component={FooView} />
-                    <Redirect from="*" to="/" />
-                </Switch>
-            </Layout>
-        </BrowserRouter>
-    );
+    const gsiEvents = useDotaEvents();
+    const timeSpan = gsiEvents.length > 0 ? gsiEvents[gsiEvents.length - 1].ts - gsiEvents[0].ts : 0;
+    console.log(timeSpan, timeSpan / 1000, Math.floor(timeSpan / 1000 / 60) + ':' + Math.floor((timeSpan / 1000) % 60));
+    return <div>
+        <h1>
+            EsportLayers GSI Simulator
+        </h1>
+    </div>
 }
